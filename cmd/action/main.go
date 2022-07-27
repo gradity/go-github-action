078ca6552f75c80340aec687d154d86a13e3bda9
Gatra <graditya@pm.me>
2022-07-27 21:52:36 +0700

    hello message

--- cmd/action/main.go
+++ cmd/action/main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("hello from docker action")
 	flag.Parse()
 	first := flag.Arg(0)
-	second := flag.Arg(1)
-	msg := fmt.Sprintf("%v %v", first, second)
+	// second := flag.Arg(1)
+	msg := fmt.Sprintf("hello %v", first)
 	fmt.Printf("::set-output name=message::%v", msg)
 }
